267be084ea0bb283b0df18ca23741ebfd7e68af7
Dr. Stefan Schade <dr_stefan_schade@yahoo.com>
2022-10-31 14:32:00 +0100

    first commit

---
+++ src/main/java/de/sterfanschade/learncompanion/LearnCompanionApplication.java
@@ -0,0 +1,13 @@
+package de.sterfanschade.learncompanion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnCompanionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnCompanionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/sterfanschade/learncompanion/LearnCompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package de.sterfanschade.learncompanion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnCompanionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
